ddc8c73af580f4ac275ac9d319edcc8bb51ab5a5
tnoell <tnoell@ucsc.edu>
2021-09-25 14:08:41 -0700

    game template

--- Assets/Scripts/GameController/Singleton.cs
+++ Assets/Scripts/GameController/Singleton.cs
@@ -44,7 +44,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
                         DontDestroyOnLoad(singletonObject);
                     }
                 }
- 
+
                 return m_Instance;
             }
         }
